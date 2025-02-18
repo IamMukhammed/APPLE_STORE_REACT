71e8225ecdc9614f526c4801ecb0670c1b7873bf
IamMukhammed <abdullaevazizkhan@gmail.com>
2025-02-18 16:32:52 -0600

    feat: Publish authentication modal component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,17 +9,25 @@ import UserPage from "./screens/userPage";
 import HomeNavbar from "./components/headers/HomeNavbar";
 import OtherNavbar from "./components/headers/OtherNavbar";
 import Footer from "./components/footer";
-import "../css/app.css";
-import "../css/navbar.css";
-import "../css/footer.css";
 import HelpPage from "./screens/helpPage";
 import Test from "./screens/Test";
 import { CartItem } from "../lib/types/search";
 import useBasket from "./hooks/useBasket";
+import AuthenticationModal from "./components/auth";
+import "../css/app.css";
+import "../css/navbar.css";
+import "../css/footer.css";
 
 function App() {
   const location = useLocation();
   const { cartItems, onAdd, onRemove, onDelete, onDeleteAll } = useBasket();
+  const [signupOpen, setSignupOpenn] = useState<boolean>(false);
+  const [loginOpen, setLoginOpen] = useState<boolean>(false);
+
+  /* HANDLERS */
+
+  const handleSignupClose = () => setSignupOpenn(false);
+  const handleLoginClose = () => setLoginOpen(false);
 
   return (
     <>
@@ -54,11 +62,17 @@ function App() {
             <HelpPage />
           </Route>
           <Route path="/">
-          {/* <Test /> */}
             <HomePage />
           </Route>
         </Switch>
         <Footer />
+
+        <AuthenticationModal 
+          signupOpen={signupOpen}
+          loginOpen={loginOpen}
+          handleSignupClose={handleSignupClose}
+          handleLoginClose={handleLoginClose}
+        />
       </>
   );
 }
