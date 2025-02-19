869622aeea1b44c86deed95a827b0cdbcdacd4b9
IamMukhammed <abdullaevazizkhan@gmail.com>
2025-02-18 20:18:02 -0600

    feat: Create useGlobals integrated with Context & develop logout process

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -17,17 +17,42 @@ import AuthenticationModal from "./components/auth";
 import "../css/app.css";
 import "../css/navbar.css";
 import "../css/footer.css";
+import { T } from "../lib/types/common";
+import { sweetErrorHandling, sweetTopSuccessAlert } from "../lib/sweetAlert";
+import { Messages } from "../lib/config";
+import MemberService from "./services/MemberService";
+import { useGlobals } from "./hooks/useGlobals";
 
 function App() {
   const location = useLocation();
+  const { setAuthMember } = useGlobals();
   const { cartItems, onAdd, onRemove, onDelete, onDeleteAll } = useBasket();
   const [signupOpen, setSignupOpenn] = useState<boolean>(false);
   const [loginOpen, setLoginOpen] = useState<boolean>(false);
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
 
   /* HANDLERS */
 
   const handleSignupClose = () => setSignupOpenn(false);
   const handleLoginClose = () => setLoginOpen(false);
+
+  const handleLogoutClick = (e: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleCloseLogout = () => setAnchorEl(null);
+  const handleLogoutRequest = async () => {
+    try {
+      const member = new MemberService();
+      await member.logout();
+
+      await sweetTopSuccessAlert("SUCCESS", 700);
+      setAuthMember(null);
+    } catch (err) {
+      console.log(err);
+      sweetErrorHandling(Messages.error1);
+    }
+  };
 
   return (
     <>
@@ -38,8 +63,12 @@ function App() {
           onRemove={onRemove} 
           onDelete={onDelete} 
           onDeleteAll={onDeleteAll}
-          setSignupOpen={setSignupOpenn}
           setLoginOpen={setLoginOpen}
+          setSignupOpen={setSignupOpenn}
+          anchorEl={anchorEl}
+          handleLogoutClick={handleLogoutClick}
+          handleCloseLogout={handleCloseLogout}
+          handleLogoutRequest={handleLogoutRequest}
         /> 
       ) : (
         <OtherNavbar
@@ -49,7 +78,11 @@ function App() {
           onDelete={onDelete} 
           onDeleteAll={onDeleteAll}
           setLoginOpen={setLoginOpen}
-          setSignupOpen={setSignupOpenn} 
+          setSignupOpen={setSignupOpenn}
+          anchorEl={anchorEl}
+          handleLogoutClick={handleLogoutClick}
+          handleCloseLogout={handleCloseLogout}
+          handleLogoutRequest={handleLogoutRequest}
         /> 
       )}
         <Switch>
