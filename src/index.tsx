39e5e8215eabbbe4f4f6de66a24bf189af047644
IamMukhammed <abdullaevazizkhan@gmail.com>
2025-06-19 01:49:56 -0500

    feat: Develop Socket.IO Client

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,31 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-import App from './app/App';
+import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
 import theme from "./app/MaterialTheme";
 import { BrowserRouter as Router } from "react-router-dom";
 import ContextProvider from "./app/context/ContextProvider";
 import { SocketProvider } from "./app/context/SocketContext";
 import "./css/index.css";
 
-const container = document.getElementById('root')!;
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>                        
+    <Provider store={store}>
       <ContextProvider>
-        <SocketProvider>                             
-        <ThemeProvider theme={theme}>               
-          <CssBaseline />                           
-          <Router>                                  
-            <App />                                 
-          </Router>
-        </ThemeProvider>
+        <SocketProvider>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Router>
+              <App />
+            </Router>
+          </ThemeProvider>
         </SocketProvider>
       </ContextProvider>
     </Provider>
