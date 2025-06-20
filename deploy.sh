220d7af35fb2ff4e0fa768012e09a4d2316b38d7
IamMukhammed <abdullaevazizkhan@gmail.com>
2025-06-19 21:52:45 -0500

    feat: Deploy project via shell & pm2

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # PRODUCTION
-# git reset --hard
-# git checkout master
-# git pull origin master
+git reset --hard
+git checkout master
+git pull origin master
 
-# npm i yarn -g
-# yarn global add serve
-# yarn
-# yarn run build
-# pm2 start "yarn run start:prod" --name=Apple-Tech-Store-React
+npm i yarn -g
+yarn global add serve
+yarn
+yarn run build
+pm2 start "yarn run start:prod" --name=Tech-Store-React
